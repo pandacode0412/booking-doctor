4156b58f7d9b43f7f032701e5ae23b8f3b015dc5
code or trader <74350275+pandacode0412@users.noreply.github.com>
2024-07-06 13:05:56 +0700

    sdfg

--- server/src/routes/web.js
+++ server/src/routes/web.js
@@ -9,7 +9,8 @@ let initWebRoutes = (app) => {
     // router.get('/crud' , homeController.get)
     router.get('/get-crud' , homeController.displayCRUD)
     // router.post('/post-crud' , homeController.postCRUD)
-    router.get("/edit-crud")
+    router.get("/edit-crud" , homeController.getEditCRUD)
+    router.post("/put-crud" , homeController.putCRUD);
 
 
     return app.use("/", router)
