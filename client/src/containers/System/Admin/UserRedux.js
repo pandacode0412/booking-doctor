e27485404133b9cb1b9cde48b041984dde5d896f
code ? <74350275+pandacode0412@users.noreply.github.com>
2024-08-17 15:57:52 +0700

    git

--- client/src/containers/System/Admin/UserRedux.js
+++ client/src/containers/System/Admin/UserRedux.js
@@ -99,21 +99,21 @@ class UserRedux extends Component {
                  <div className="user-redux-body">
                      <div className="container">
                         <div className="row">
-                            <div className="col-12"></div>
+                            <div className="col-12"><FormattedMessage id="manage-user.add" /></div>
                           <div className="col-3">
-                                 <label>Email</label>
+                                 <label><FormattedMessage id="manage-user.email" /></label>
                                  <input className="form-control" type="email" />
                           </div>
                           <div className="col-3">
-                                 <label>Password</label>
+                                 <label><FormattedMessage id="manage-user.password" /></label>
                                  <input className="form-control" type="password" />
                           </div>
                           <div className="col-3">
-                                 <label>First name</label>
+                                 <label><FormattedMessage id="manage-user.first-name" /></label>
                                  <input className="form-control" type="text" />
                           </div>
                           <div className="col-3">
-                                 <label>Last name</label>
+                                 <label><FormattedMessage id="manage-user.add" /></label>
                                  <input className="form-control" type="text" />
                           </div>
                           <div className="col-3">
@@ -142,7 +142,9 @@ class UserRedux extends Component {
                                  <label>Image</label>
                                  <input type="text" className="form-control" />
                           </div>
-                          <button className="btn btn-primary">Save</button>
+                          <div className="col-12">
+                               <button className="btn btn-primary">Save</button>
+                          </div>
                         </div>
                      </div>
                  </div>
